146d2e8e7eed659cf04b5335fd012ea62b2ff2d5
MUHAMMAD HASSAAN TAHIR <hassaantahir991@gmail.com>
2024-12-11 09:16:41 +0500

    Create string.dart

---
+++ string.dart
@@ -0,0 +1,17 @@
+void main() {
+  // String is a reserved keyword
+  // First we will use string datatype and print some text
+
+  String word = 'Hello my name is Hassaan';
+
+  print(word);
+
+  // Now we will find the total lenth of these words
+
+  print(word.length);
+
+  // Every word of string consider an index and it start from 0
+  // Now we want to print any alphabet then we will use this
+
+  print(word[12]);
+}
